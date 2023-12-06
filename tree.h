d64b72d59ae6681fafb662fa87ae96494b8ab33b
ibtehaj421 <114730186+ibtehaj421@users.noreply.github.com>
2023-12-07 00:03:10 +0500

    Ver4.1 tree.h
    Added the keylist structure which now caters to the same hash values.

--- tree.h
+++ tree.h
@@ -5,28 +5,102 @@ using namespace std;
 
 //key struct
 //the key structure is required to store a hash and a string value of the item name.
-struct key {
+struct keyNode {
 	int hash; //using hash as a standalone for now with a null value.
 	string itemName; //item name is the name of the file we will have to search for.
+	keyNode* next; //the next pointer in the key node to point to the next node in the key list class.
 	//for the default constructor we will initialize both strings to be empty.
 	void keySetDetails(int a = -1, string b = " ") {
 		hash = a;
 		itemName = b;
 	}
 };
-
+class KeyList {
+public:
+	keyNode* head; //root point to the start of the list.
+	int counter; //counter variable that helps keep track of how many inserted elements.
+	KeyList() {
+		//default constructor that will allocate null value to root and set counter to zero.
+		head = NULL;
+		counter = 0;
+	}
+	bool isEmpty() {
+		if (head == NULL) {
+			return true;
+		}
+		return false;
+	}
+	//insert a new node
+	void insert(int hash, string file) {
+		
+		if (isEmpty()) {
+			head = new keyNode;
+			head->hash = hash;
+			head->itemName = file;
+			head->next = NULL;
+			counter++;
+		}
+		else {
+			keyNode* add = new keyNode;
+			add->hash = hash;
+			add->itemName = file;
+			add->next = NULL;
+			keyNode* shift = head;
+			while (shift->next != NULL) {
+				shift = shift->next;
+			}
+			shift->next = add;
+			add = NULL;
+			delete add;
+			counter++;
+		}
+		
+	}
+	void print() {
+		keyNode* disp = head;
+		for (int i = 0; i < counter; i++) {
+			cout << disp->itemName;
+			cout << endl;
+			disp = disp->next;
+		}
+	}
+	void Remove() { //FIFO
+		
+		keyNode* temp = new keyNode;
+		temp = head;
+		head = head->next;
+		counter--;
+		delete temp;
+		
+	}
+	void clear() {
+		int sizeTemp = counter;
+		for (int i = 0; i < sizeTemp; i++) {
+			 Remove();
+		}
+	}
+	KeyList operator =(KeyList& input) {
+		this->clear();
+		keyNode* point = input.head;
+		while (point != NULL) {
+			this->insert(point->hash,point->itemName);
+			point = point->next;
+		}
+		return *this;
+	}
+};
 //b tree node
 //contains a structure array of the key.
 //contains a pointer array of the children subject to degree of the tree.
 class bTreeNode {
 public:
-	key* keys; //the keyy array
+	KeyList* keys; //the keyy array
 	int degree; //the degree specifier per node.
 	int keyNum; //the current number of keys present in a node.
 	bTreeNode** children; //the children pointer array that will store the next pointer added to the tree.
 	bool leaf; //indicator if a node is leaf node or no.
 	bTreeNode(int deg, bool isLeaf) {
-		keys = new key[2 * deg - 1];
+		keys = new KeyList[2 * deg - 1];
 		degree = deg;
 		keyNum = 0;
 		children = new  bTreeNode * [2 * degree];
@@ -43,39 +117,51 @@ class bTree {
 public:
 	bTreeNode* root; //root node of the tree. will help in growing the tree.
 	int tDeg; //degree of the tree that can be made.
+	int hashFound; //a index that will be used for searching a duplicate hash value and if found it will add it to the keys linked list.
 	bTree(int degree) { //constructor for the tree. Since we need to specify the size during the run time for creation.
 		root = NULL;
 		tDeg = degree;
+		hashFound = 0;
 	}
 	void insert(int hash, string item) {
 		//initializer if the tree is empty.
 		if (root == NULL) {
 			root = new bTreeNode(tDeg, true);
-			root->keys[0].itemName = item;
-			root->keys[0].hash = hash;
+			root->keys[0].insert(hash, item);
 			root->keyNum = 1;
 		}
 		//if the root is full we will proceed to grow the tree.
 		else if (root->keyNum == 2 * tDeg - 1) {
 			//create a new node , let this be called the calling node through which we will reset out structure.
-			bTreeNode* callinNode = new bTreeNode(tDeg, false);
-			//make old root into a child of the new root
-			callinNode->children[0] = root;
-			//split the root.
-			//through the calling node.
-			split(callinNode, 0, root);
-
-			int i = 0;
-			if (callinNode->keys[0].hash < hash) {
-				i++;
+			if (searchForHash(root, hash)) {
+				root->keys[hashFound].insert(hash, item);
 			}
-			NotFull(callinNode->children[i], hash, item);
+			else {
+				bTreeNode* callinNode = new bTreeNode(tDeg, false);
+				//make old root into a child of the new root
+				callinNode->children[0] = root;
+				//split the root.
+				//through the calling node.
+				split(callinNode, 0, root);
 
-			root = callinNode;
+				int i = 0;
+				if (callinNode->keys[0].head->hash < hash) {
+					i++;
+				}
+				NotFull(callinNode->children[i], hash, item);
+
+				root = callinNode;
+			}
 		}
 		else {
 			//root is not full so we are inserting into the root.
-			NotFull(root, hash, item);
+			//first checking if the hash already exists in the root.
+			if (searchForHash(root,hash)) {
+				root->keys[hashFound].insert(hash, item);
+			}
+			else {
+				NotFull(root, hash, item);
+			}
 		}
 	}
 	//method to split children
@@ -86,8 +172,8 @@ public:
 		newNode->keyNum = caller->degree - 1;
 		//copying keys of internal node to the newNode
 		for (int i = 0; i < caller->degree - 1; i++) {
-			newNode->keys[i].hash = input->keys[i + caller->degree].hash;
-			newNode->keys[i].itemName = input->keys[i + caller->degree].itemName;
+			newNode->keys[i] = input->keys[i + caller->degree];
+			
 		}
 		//reducing the number of keys in the input node by 1. Since it was full it will go down by 1.
 		input->keyNum = caller->degree - 1;
@@ -102,13 +188,13 @@ public:
 		//the caller node will have a key of input be transfered to it.
 		//shifting values so that space is created for that value.
 		for (int i = (caller->degree - 1); i >= cIndex; i--) {
-			caller->keys[i + 1].hash = caller->keys[i].hash;
-			caller->keys[i + 1].itemName = caller->keys[i].itemName;
+			caller->keys[i + 1] = caller->keys[i];
+			
 		}
 
 		//adding that value to the calling node.
-		caller->keys[cIndex].hash = input->keys[caller->degree - 1].hash;
-		caller->keys[cIndex].itemName = input->keys[caller->degree - 1].itemName;
+		caller->keys[cIndex] = input->keys[caller->degree - 1];
+		
 
 		caller->keyNum += 1;
 	}
@@ -119,35 +205,45 @@ public:
 
 		//check if leaf node or not.
 		if (adder->leaf) {
-			//shifting all greater values to the right.
-			while (index >= 0 && adder->keys[index].hash > hash) {
-				adder->keys[index + 1].hash = adder->keys[index].hash;
-				adder->keys[index + 1].itemName = adder->keys[index].itemName;
-				index--;
+			if (searchForHash(adder, hash)) {
+				adder->keys[hashFound].insert(hash, item);
 			}
-			//inserting the new key.
-			adder->keys[index + 1].hash = hash;
-			adder->keys[index + 1].itemName = item;
-			adder->keyNum += 1;
+			else {
+				//shifting all greater values to the right.
+				while (index >= 0 && adder->keys[index].head->hash > hash) {
+					adder->keys[index + 1] = adder->keys[index];
+					index--;
+				}
+				//inserting the new key.
+				adder->keys[index + 1].clear();
+				adder->keys[index + 1].insert(hash,item);
+				adder->keyNum += 1;
+			}
 		}
 		//if not leaf node.
 		else {
-			//find index without shifting.
-			while (index >= 0 && adder->keys[index].hash > hash) {
-				index--;
+			//check if index exists
+			if (searchForHash(adder, hash)) {
+				adder->keys[hashFound].insert(hash, item);
 			}
-			//check if the child at the current required index is full or not.
-			if (adder->children[index + 1]->keyNum == (2 * adder->degree) - 1) {
-				split(adder, index + 1, adder->children[index + 1]);
-				//see which child is going to have the new key the smaller batch or the greater batch.
-				if (adder->keys[index + 1].hash > hash) {
-					index++;
+			else {
+				//find index without shifting.
+				while (index >= 0 && adder->keys[index].head->hash > hash) {
+					index--;
 				}
+				//check if the child at the current required index is full or not.
+				if (adder->children[index + 1]->keyNum == (2 * adder->degree) - 1) {
+					split(adder, index + 1, adder->children[index + 1]);
+					//see which child is going to have the new key the smaller batch or the greater batch.
+					if (adder->keys[index + 1].head->hash > hash) {
+						index++;
+					}
 
+				}
+				//calling an insert for the child now.
+				//insert the new key into the child.
+				NotFull(adder->children[index + 1], hash, item);
 			}
-			//calling an insert for the child now.
-			//insert the new key into the child.
-			NotFull(adder->children[index + 1], hash, item);
 		}
 	}
 	//traversing or searching.
@@ -157,7 +253,8 @@ public:
 			if (!path->leaf) {
 				traverse(path->children[i]);
 			}
-			cout << " " << path->keys[i].itemName << endl;
+			cout << "index: " << i << endl;
+			path->keys[i].print();
 		}
 		//print sub tree
 		if (!path->leaf) {
@@ -168,28 +265,45 @@ public:
 	//search in the tree
 	bool search(bTreeNode* find, int hash, string& path) {
 		int i = 0; //searching index
-		while (i<find->keyNum && hash > find->keys[i].hash) {
+		while (i<find->keyNum && hash > find->keys[i].head->hash) {
 			i++;
 		}
-		if (hash == find->keys[i].hash) {
-			path = path.append(find->keys[i].itemName);
+		if (hash == find->keys[i].head->hash) {
+			
 			return true;
 		}
 		if (find->leaf) {
 			return false;
 		}
-		path = path.append(find->keys[i].itemName);
-		path = path.append("/");
+		
 		return search(find->children[i], hash, path);
 	}
-
+	bool searchForHash(bTreeNode* find,int hash) {
+		int i = 0;
+		while (i<find->keyNum && hash > find->keys[i].head->hash) {
+			i++;
+		}
+		if (find->keys[i].head != NULL) {
+			if (hash == find->keys[i].head->hash) {
+				hashFound = i;
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+		
+	}
 	//deleting files and folders. //Need to ensure that the node does not get too small during deletion.
 	void Delete(bTreeNode* input,int hash) {
 		//finding the index of the key that has to be removed.
 		int indexKey = getIndexOfKey(input, hash);
 
 		//if key to be removed is present in one of the current node indexes.
-		if (indexKey < input->keyNum && input->keys[indexKey].hash == hash) {
+		if (indexKey < input->keyNum && input->keys[indexKey].head->hash == hash) {
 			if (input->leaf) {
 				LeafRemove(input, indexKey); //remove the value from the leaf
 			}
@@ -223,28 +337,26 @@ public:
 	}
 	void LeafRemove(bTreeNode* input,int index) { //remove the value from a leaf.
 		for (int i = index + 1; i < input->keyNum; i++) {
-			input->keys[i - 1].hash = input->keys[i].hash;
-			input->keys[i - 1].itemName = input->keys[i].itemName;
+			input->keys[i - 1] = input->keys[i];
+			
 		}
 		input->keyNum--;
 	}
 	void InternalRemove(bTreeNode* input,int index) { //remove a value from an internal node
 		//getting a hash value to be used.
-		int tempHash = input->keys[index].hash;
+		int tempHash = input->keys[index].head->hash;
 		//if child has atleast t keys.
 		if (input->children[index]->keyNum >= input->degree) {
-			key predecessor = getPredecessor(input, index); //get the predecessor. contains both hash and itemName.
-			input->keys[index].hash = predecessor.hash;
-			input->keys[index].itemName = predecessor.itemName;
-			Delete(input->children[index],predecessor.hash);
+			KeyList predecessor = getPredecessor(input, index); //get the predecessor. contains both hash and itemName.
+			input->keys[index] = predecessor;
+			Delete(input->children[index],predecessor.head->hash);
 		}
 		//examine the next child if the previous child does not meet the criteria.
 		else if (input->children[index + 1]->keyNum >= input->degree) {
 			//get successor.
-			key successor = getSuccessor(input, index);
-			input->keys[index].hash = successor.hash;
-			input->keys[index].itemName = successor.itemName;
-			Delete(input->children[index], successor.hash);
+			KeyList successor = getSuccessor(input, index);
+			input->keys[index] = successor;
+			Delete(input->children[index], successor.head->hash);
 		}
 		else {
 			//calling the merge function.
@@ -252,7 +364,7 @@ public:
 			Delete(input->children[index], tempHash);
 		}
 	}
-	key getPredecessor(bTreeNode* input,int index) { //retrieve the value of the predecessor. Of a certain key or hash
+	KeyList getPredecessor(bTreeNode* input,int index) { //retrieve the value of the predecessor. Of a certain key or hash
 		bTreeNode* finder = input->children[index];
 		while (!finder->leaf) {
 			finder = finder->children[finder->keyNum]; //keep going right till value of that index is specified at a leaf node.
@@ -260,7 +372,7 @@ public:
 
 		return finder->keys[finder->keyNum - 1];
 	}
-	key getSuccessor(bTreeNode* input,int index) { //retireve the value of a successor of a certain key or hash.
+	KeyList getSuccessor(bTreeNode* input,int index) { //retireve the value of a successor of a certain key or hash.
 		bTreeNode* finder = input->children[index + 1];
 		while (!finder->leaf) {
 			finder = finder->children[0]; //keep moving left till you reach the required left most value at a leaf node.
@@ -286,16 +398,14 @@ public:
 	void borrowNext(bTreeNode*input,int index) { //borrow a value from the i+1 sibling.
 		bTreeNode* child = input->children[index];
 		bTreeNode* sibling = input->children[index + 1];
-		child->keys[child->keyNum].hash = input->keys[index].hash;
-		child->keys[child->keyNum].itemName = input->keys[index].itemName;
+		child->keys[child->keyNum] = input->keys[index];
 
 		//first child of the sibling becomes the last child of the currently chosen child.
 		if (!child->leaf) {
 			child->children[child->keyNum + 1] = sibling->children[0];
 		}
 	//key from subling is inserted into the main node.
-		input->keys[index].hash = sibling->keys[0].hash;
-		input->keys[index].itemName = sibling->keys[0].itemName;
+		input->keys[index] = sibling->keys[0];
 
 	 //shifting all keys of sibling to fill the emptied space.
 		for (int i = 1; i < sibling->keyNum; i++) {
@@ -370,13 +480,13 @@ public:
 		child->keyNum += sibling->keyNum;
 		input->keyNum--;
 		delete sibling;
+		sibling = NULL;
 	}
 	int getIndexOfKey(bTreeNode* input, int hash) {
 		int index = 0;
-		while (index < input->keyNum && input->keys[index].hash < hash) {
+		while (index < input->keyNum && input->keys[index].head->hash < hash) {
 			index++;
 		}
 		return index;
 	}
 };
-//an updated updated ver. 
