d62c363414ab2e0353976d14a63c31de2d12be0b
ibtehaj421 <114730186+ibtehaj421@users.noreply.github.com>
2023-12-14 15:31:40 +0500

    Update driverCode.cpp

--- driverCode.cpp
+++ driverCode.cpp
@@ -1,19 +1,28 @@
-#include "tree.h"
+#include "btreemain.h"
 
-//run the tests here
+
 int main() {
-    bTree t(3);
-    t.insert(1,"string1");
-    t.insert(3,"string2");
-    t.insert(7,"string3");
-    t.insert(10,"string4");
-    t.insert(11,"string5");
-    t.insert(13,"string6");
-    t.traverse(t.root);
-    t.Delete(t.root, 10);
-    t.Delete(t.root, 13);
-    cout << endl;
-    cout << "After deletions: " << endl;
-    t.traverse(t.root);
-	return 0;
+    bTree t(4);
+    t.insert2(1, "string1");
+    //t.insert2(1, "string11");
+    t.insert2(2, "string2");
+    t.insert2(3, "string3");
+    t.insert2(4, "string4");
+    t.insert2(5, "string5");
+    t.insert2(6, "string6");
+    t.insert2(7, "string7");
+    t.insert2(8, "string8");
+    t.insert2(9, "string9");
+    t.insert2(3, "string31");
+    //t.Delete(t.root, 3);
+    //t.insert(10, "string10");
+    //t.insert(11, "string11");
+    //t.insert(13, "string13");
+    t.Display(t.root);
+    //t.Delete(t.root, 10);
+    //t.Delete(t.root, 13);
+    //cout << endl;
+    //cout << "After deletions: " << endl;
+   // t.traverse(t.root);
+    return 0;
 }
