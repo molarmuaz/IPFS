405bba5f82618ead8504f0924a3adff7be92beab
Muhammad Muaz Ahmed <112881407+molarmuaz@users.noreply.github.com>
2023-12-15 19:56:54 +0500

    Update network.h
    added file inserts and removes
    
    fixed node inserts and removes
    
    added a search function

--- network.h
+++ network.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "node.h"
 #include <time.h>
 #include <cmath>
@@ -12,134 +13,223 @@ private:
     int numberOfPCs;
     int IDspace;
     int activePCs;
-    bool * activeIDs;
+    bool* activeIDs;
+    int degree;
 public:
-    Network(int idsp)
+    Network(int idsp, int deg)
     {
         IDspace = idsp;
+        degree = deg;
         head = NULL;
-        numberOfPCs = pow(2,IDspace);
+        numberOfPCs = pow(2, IDspace);
         activePCs = 0;
         //making a hash table to store IDs of active PCs
-        activeIDs = new bool[numberOfPCs]{0};
+        activeIDs = new bool[numberOfPCs] {0};
     }
 
     void activateamount(int x)
     {
-        for(int i = 0; i<x; i++)
+        for (int i = 0; i < x; i++)
         {
-            activate();
+            insert();
         }
     }
 
-    void activate(int x = -1)
+    void insert(int x = -1)
     {
         srand(time(NULL));
         //for now id is just a random number generated between 0 and numberOfPCs and is a number that isn't already active
-        if(activePCs < numberOfPCs)
+        if (activePCs < numberOfPCs && x)
         {
-            if(x == -1 && activePCs)
+            if (x == -1 && activePCs)
             {
                 //finding a random pc to activate
                 x = rand() % activePCs;
-                while(activeIDs[x] != 0)
+                while (activeIDs[x] != 0)
                 {
                     x = rand() % activePCs;
                 }
             }
-            else if(!activePCs)
+            else if (!activePCs)
             {
-                
+                x = 0;
             }
 
-            node* temp = new node;
+            node* temp = new node(degree);
             temp->setID(x);
 
-            node* p = head;
-            //adding the pc//
-            while(true)
+            if (!activePCs)
             {
-                if(p->getID() < x && p->getNext()->getID() > x)
-                {
-                    break;
-                }
-                p = p->getNext();
+                head = temp;
+                head->setNext(head);
+                temp->setStart(0);
             }
+            else
+            {
+                node* p = head;
+                //adding the pc//
+                while (true)
+                {
+                    if (p->getID() < x && p->getNext()->getID() > x)
+                    {
+                        break;
+                    }
+                    p = p->getNext();
+                }
 
-            temp->setNext(p->getNext());
-            p->setNext(temp);
+                temp->setNext(p->getNext());
+                p->setNext(temp);
 
+                //Insert range
+                temp->setStart(p->getID() + 1);
+
+                //Update routingtables
+                p = p->getNext();
+                RoutingTable tempRouter;
+                while (p->getNext() != temp)
+                {
+                    p = p->getNext();
+                    p->getRouter().insertEntry(temp);
+                }
+
+                FileList hashes;
+                temp->getNext()->getDir().Transfer(temp->directory,temp->getStart(), temp->getEnd(),IDspace, hashes);
+                temp->getDir().RemoveRange(hashes);
+                hashes.clear();
+
+            }
             //recording in table that PC x has been activated
             activeIDs[x] = true;
             activePCs++;
         }
         else
         {
-            cout<<"Max limit reached"<<endl;
-        }
-
-        //INITIALIZE ITS ROUTING TABLE
-        //UPDATE THE ROUTING TABLE OF THE REST OF THE NODES
-        
-        //ADD FUNCTIONALITY TO BALANCE FILE DISTRIBUTION AFTER ADDITION OF A PC (AFTER B-TREES ARE MADE)
+            cout << "Max limit reached" << endl;
+        }    
     }
 
-    int hashing(string hash, int max = -1 )
+    int hashing(string hash, int max = -1)
     {
         char x;
         int result = 0;
-        for(int i =0; i<3; i++)
+        for (int i = 0; i < 3; i++)
         {
-            x = hash[2-i];
-            if(x>=97 && x<=102)
+            x = hash[2 - i];
+            if (x >= 97 && x <= 102)
             {
-                result += (int(x)-87) * pow(16,i);
+                result += (int(x) - 87) * pow(16, i);
             }
             else
             {
-                result+= (int(x)-48) * pow(16,i);
+                result += (int(x) - 48) * pow(16, i);
             }
         }
 
-        if(max != -1)
+        if (max != -1)
         {
-            return (result%max);
+            int id = pow(2, max);
+            return (result % id);
         }
         return result;
     }
 
+    string readFileToString(ifstream& inputStream) {
 
-    void insert(string x) // we can later create a function to read files and then insert the contents of that file in this function as a string
-    {
-        int key; // UNSURE OF WHAT METHOD TO USE TO ASSIGN KEYS | IF WE ARE USING A LINEAR COUNTER TO ASSIGN KEYS findNode() works fine
-        int nodeAssigned;
+        string line;
+        string content;
 
-        if(key > numberOfPCs || key < 0)
-        {
-            nodeAssigned = 0;
+        // Check if file opened successfully
+        if (!inputStream.is_open()) {
+            throw  runtime_error("Failed to open file.");
         }
-        else if(activeIDs[key])
-        {
-            nodeAssigned = key;
+
+        // Read line by line and append to content
+        while (getline(inputStream, line)) {
+            content += line;
         }
-        else
-        {
-            nodeAssigned = key;
-            while(!activeIDs[nodeAssigned])
-            {
-                nodeAssigned++;
-            }           
+
+        // Close the file stream
+        inputStream.close();
+
+        return content;
+    }
+
+    string getSHA1HashFile(ifstream& inputStream) {
+
+        const  string& data = readFileToString(inputStream);
+        // Create the SHA1 context
+        EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+        int result = EVP_DigestInit_ex(ctx, EVP_sha1(), NULL);
+
+        // Update the SHA1 context with the data
+        EVP_DigestUpdate(ctx, data.c_str(), data.length());
+
+        // Calculate the final SHA1 hash
+        unsigned char hash[EVP_MAX_MD_SIZE];
+        unsigned int hash_len;
+        EVP_DigestFinal_ex(ctx, hash, &hash_len);
+
+        // Convert the hash to a hexadecimal string
+        stringstream ss;
+        for (int i = 0; i < SHA_DIGEST_LENGTH; ++i) {
+            ss << hex << setfill('0') << setw(2) << (int)hash[i];
         }
-        while(head->getID() != nodeAssigned)
-        {
-            head = head->getNext();
-        }
-        head->insert(x); // IBBI AND YAN'S WORK
+
+        // Clean up the SHA1 context
+        EVP_MD_CTX_free(ctx);
+
+        return ss.str();
     }
 
     void remove(int id)
     {
-        //AFTER B TREES ARE DESIGNED
+        node* a = head;
+        while (a->getID() != id)
+        {
+            a = a->getNext();
+        }
+        node* b = a;
+        while (true)
+        {
+            b->getRouter().deleteEntry(id);
+            if (b->getNext() != a)
+            {
+                break;
+            }
+            b = b->getNext();
+        }
+        FileList hashes;
+        a->getNext()->getDir().Transfer(a->directory, a->getStart(), a->getEnd(), IDspace, hashes);
+        a->getDir().RemoveRange(hashes);
+        hashes.clear();
+        b->setNext(a->getNext());
+        delete a;
     }
 
+    void insertFile(ifstream& path) // we can later create a function to read files and then insert the contents of that file in this function as a string
+    {
+
+        string hash = getSHA1HashFile(path);
+        int key = hashing(hash);
+        node* a = head->getRouter().findNode(key);
+
+
+        a->insert(path, hash);
+    }
+
+    void removeFile(string hash)
+    {
+        int key = hashing(hash, IDspace);
+        node* a = head->getRouter().findNode(key);
+
+        a->remove(hash);
+    }
+
+    string search(string hash)
+    {
+        int key = hashing(hash, IDspace);
+        node* a = head->getRouter().findNode(key);
+
+        return a.search(hash);
+    }
 };
