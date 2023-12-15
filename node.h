7ad10216b2e87c417f1a5d87606cc99a07b8dc67
Muhammad Muaz Ahmed <112881407+molarmuaz@users.noreply.github.com>
2023-12-15 19:57:07 +0500

    Update node.h

--- node.h
+++ node.h
@@ -1,15 +1,38 @@
-#include "tree.h"
+#pragma once
+#include "maxtree.h"
+#include "RoutingTable.h"
+
+
+struct fileRange
+{
+    int start;
+    int end;
+};
+
+
 class node
 {
 private:
+    fileRange range;
     
-    bTree head;
     node* next;
     int id;
+    RoutingTable router;
 public:
-    node()
+    bTree directory;
+    node(int deg): directory(deg), router(this)
     {
         next = nullptr;
+    }
+
+    bTree getDir()
+    {
+        return directory;
+    }
+
+    RoutingTable getRouter()
+    {
+        return router;
     }
 
     void setID(int idn)
@@ -27,13 +50,55 @@ public:
         next = n;
     }
 
-    node * getNext()
+    node* getNext()
     {
         return next;
     }
 
-    void insert(string x)
+    void insert(ifstream& path, string hash)
     {
+        ostringstream out;
 
+        out << path.rdbuf();
+
+        string s = out.str();
+        directory.insert2(hash, s);
+    }
+
+    void remove(string hash)
+    {
+        directory.remove(hash);
+    }
+
+    string search(string hash)
+    {
+        string path;
+        int a = 0;
+        if(!directory.ExtractFiles(directory.root, hash, path,a))
+        {
+            path = "FILE NOT FOUND";
+        }
+        return path;
+    }
+    
+    fileRange getRange() 
+    {
+        return range;
+    }
+
+    int getStart()
+    {
+        return range.start;
+    }
+
+    int getEnd()
+    {
+        return range.end;
+    }
+
+    void setStart(int a) 
+    {
+        range.start = a;
+        range.end = id;
     }
 };
